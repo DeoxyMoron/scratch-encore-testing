8957b5c200436c40e73323377d37ff2b8edfaf5c
DeoxyMoron <dsanosa@gmail.com>
2017-07-18 16:08:25 -0700

    test

---
+++ GoogleClassroomTesting/test.py
@@ -0,0 +1,16 @@
+import sys
+print(sys.executable)
+print(sys.version)
+
+
+def foo():
+    print("lmao")
+
+
+def bar():
+    print("heheheh")
+    print("wooo")
+
+
+for num in [1, 2, 3, 4]:
+    print(num)
